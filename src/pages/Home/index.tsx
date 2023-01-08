3b6f3361f30e978a3d00cf2479f1737048a497d1
Jcmendes7 <josecarlosms7f@gmail.com>
2023-01-08 10:48:01 -0300

    projeto ignite timer

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou Trabalhar em.</label>
+          <TaskInput 
+            id="task" 
+            list="task-suggestion" 
+            placeholder="Dé um nome para o seu projeto" 
+          />
+
+          <datalist id="task-suggestion">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Jogar Game" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">minutos</label>
+          <MinutesAmountInput 
+            type="number" 
+            id="minutesAmount" 
+            placeholder="00" 
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
